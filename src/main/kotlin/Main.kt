b128194d6676f758f8aaf5040200053fb821915d
Mikhaaaaa <7links91@mail.ru>
2023-03-26 21:28:29 +0300

    Самая лучшая попытка

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,39 @@
-import java.util.Scanner
-import java.util.SplittableRandom
+import java.util.*
 
 fun main() {
     start()
 }
 
 
-
-fun start(){
-   val navigation = Navigation()
-   navigation.snowFirstScreen()
-   navigation.chooseStep()
-
+fun start() {
+    println("Добро пожаловать!")
+    val archive = Archives()
+    archive.step()
+    archive.navigate(getCommandConsole())
 }
 
 
+fun stop(){
+    println("Спасибо что воспользовались программой по хранению заметок")
+}
+fun getCommandConsole(): Int {
 
-fun readNote(){
+    var console = Scanner(System.`in`)
+    while (true) {
+        if (console.hasNextInt()) {
+            var a = console.nextInt()
+            when(a){
+                1 -> return 1
+                2 -> return 2
+                3 -> return 3
+                else -> println("Введите число от 1 до 3!!!!")
+            }
+        } else {
+            println("Введите число от 1 до 3!!!!")
+            console.next()
+        }
+    }
+
 
 }
 
